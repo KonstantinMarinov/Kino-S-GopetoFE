6b402b589e85d559a054045a5e401871281c7fb1
moka <konstantin.marinov17@zuhlke.com>
2022-12-09 11:57:48 +0200

    feat: added improvements to the movie feature, added image in the template

--- src/app/movies/movie/movie.component.ts
+++ src/app/movies/movie/movie.component.ts
@@ -1,30 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IMovie } from './movie.interface';
-import { MovieService } from '../movies.service';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
 })
 export class MovieComponent implements OnInit {
-  movies!: IMovie[];
+  @Input() movies!: IMovie[];
 
-  constructor(private movieService: MovieService) {}
-
-  ngOnInit(): void {
-    console.log(this.movies);
-
-    this.getMovies();
-  }
-
-  getMovies() {
-    this.movieService.getMovies().subscribe({
-      next: (response) => {
-        this.movies = response;
-      },
-      error: (error) => {
-        console.log(error);
-      },
-    });
-  }
+  ngOnInit(): void {}
 }
