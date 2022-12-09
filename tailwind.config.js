48e141e5c0809faf3e418d3ff6e3c91f09658295
moka <konstantin.marinov17@zuhlke.com>
2022-12-09 19:32:15 +0200

    feat: dummy navbar created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      black: "#141414",
+      white: "#e5e5e5",
+      lightBlue: "#00b4d8",
+      superLightBlue: "#caf0f8",
+    },
     extend: {},
   },
   plugins: [],
