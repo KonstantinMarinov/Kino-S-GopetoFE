97913ee2b3719bb390c38c456300abd9414d70c7
moka <konstantin.marinov17@zuhlke.com>
2022-12-09 11:57:48 +0200

    feat: added improvements to the movie feature, added image in the template

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,11 +1,12 @@
+import { MoviePageComponent } from './movie-page.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
-  declarations: [MovieComponent],
+  declarations: [MovieComponent, MoviePageComponent],
   imports: [CommonModule],
-  exports: [MovieComponent],
+  exports: [MovieComponent, MoviePageComponent],
   providers: [],
 })
 export class MovieModule {}
